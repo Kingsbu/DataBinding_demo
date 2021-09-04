4d15b2d0d408a2e8472a9e8337d42dc73800f64f
Kingsbu <sibusisok32@gmail.com>
2021-09-04 15:51:58 +0200

    Initial Project commit

---
+++ app/src/main/java/com/sibusiso/databinding_demo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.sibusiso.databinding_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sibusiso.databinding_demo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSubmit.setOnClickListener{
+            greetingMessage()
+        }
+    }
+
+    private fun greetingMessage() {
+        binding.textView.text = binding.editTextPersonName.text
+    }
+}
---
+++ app/src/main/java/com/sibusiso/databinding_demo/Student.kt
@@ -0,0 +1,8 @@
+package com.sibusiso.databinding_demo
+
+data class Student (
+    var id: Int,
+    var name: String,
+    var email: String
+    )
+
